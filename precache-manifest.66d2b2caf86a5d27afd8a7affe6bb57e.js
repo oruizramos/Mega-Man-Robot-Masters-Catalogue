dd91f391b3ecb2c1eb018f59ceef30655d264987
oruizramos <omarruizramos@gmail.com>
2020-10-27 14:55:30 +0100

    Updates

---
+++ precache-manifest.66d2b2caf86a5d27afd8a7affe6bb57e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b220e79a7d96a9837a3ac9efc5ed571",
+    "url": "/Mega-Man-Robot-Masters-Catalogue/index.html"
+  },
+  {
+    "revision": "81598ed9cac47599b546",
+    "url": "/Mega-Man-Robot-Masters-Catalogue/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "77f4d0c300277fa2dee0",
+    "url": "/Mega-Man-Robot-Masters-Catalogue/static/css/main.7d7c5028.chunk.css"
+  },
+  {
+    "revision": "81598ed9cac47599b546",
+    "url": "/Mega-Man-Robot-Masters-Catalogue/static/js/2.69390f2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mega-Man-Robot-Masters-Catalogue/static/js/2.69390f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f4d0c300277fa2dee0",
+    "url": "/Mega-Man-Robot-Masters-Catalogue/static/js/main.e0976e9a.chunk.js"
+  },
+  {
+    "revision": "73e6d3deb8c33a561133",
+    "url": "/Mega-Man-Robot-Masters-Catalogue/static/js/runtime-main.0534a39a.js"
+  },
+  {
+    "revision": "1b86d507f08b980d3515b6a9eea8517c",
+    "url": "/Mega-Man-Robot-Masters-Catalogue/static/media/megaman_2.1b86d507.eot"
+  },
+  {
+    "revision": "227f68ac27cd2f5d6f89246f58abd209",
+    "url": "/Mega-Man-Robot-Masters-Catalogue/static/media/megaman_2.227f68ac.svg"
+  },
+  {
+    "revision": "6d5b21a1d9130cb9fb2b10c2322049ac",
+    "url": "/Mega-Man-Robot-Masters-Catalogue/static/media/megaman_2.6d5b21a1.ttf"
+  },
+  {
+    "revision": "a57a3c0c102c29c947c918ef8e6b92bf",
+    "url": "/Mega-Man-Robot-Masters-Catalogue/static/media/megaman_2.a57a3c0c.woff"
+  }
+]);
